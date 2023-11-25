8730d6cf6d6423a68d9d2eb66b73aa17d18829bb
Magnus Wallin <magnuswallin@tutanota.com>
2023-11-25 18:39:19 +0100

    Use external function

--- 02-unseal-vault.sh
+++ 02-unseal-vault.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
+# Get unseal function
+source unseal.sh
+
 export VAULT_ADDR="http://localhost:8210"
-
-i=0
-
-while read -r line; do
-    if (( i < 3 )); then
-        if [[ "$line" =~ ^Unseal ]]; then
-            key=$(echo "$line" | awk '{ print $4 }')
-        fi
-        vault operator unseal "$key"
-        ((i++))
-    fi
-done < vault-init.txt
+unseal
