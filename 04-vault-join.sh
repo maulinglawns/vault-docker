6ff42cddd04f45562be44d5acef1c10f3c7a7e15
Magnus Wallin <magnuswallin@tutanota.com>
2023-11-25 18:40:06 +0100

    Join and unseal Vault
    We now both join and unseal the remaining two nodes,
    so there is no need for manual interaction.

--- 04-vault-join.sh
+++ 04-vault-join.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Get unseal function
+source unseal.sh
+
 leader="http://vault-1:8200"
 
-VAULT_ADDR="http://localhost:8220" vault operator raft join "$leader"
-VAULT_ADDR="http://localhost:8230" vault operator raft join "$leader"
+export VAULT_ADDR="http://localhost:8220"
+vault operator raft join "$leader"
+unseal
+
+export VAULT_ADDR="http://localhost:8230"
+vault operator raft join "$leader"
+unseal
